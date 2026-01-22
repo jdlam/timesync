5811b49e48d95fa2e53d038e4318b055732e00af
Jon Lam <jonlam92@gmail.com>
2026-01-22 09:39:32 +0800

    Revert "Chore: Fix Biome lint warnings (#25)"
    This reverts commit f73a8b2aa5a67e9e60a2245ecbc277ad08cf4ffe.

--- src/components/availability-grid/AvailabilityGrid.tsx
+++ src/components/availability-grid/AvailabilityGrid.tsx
@@ -228,8 +228,8 @@ export function AvailabilityGrid({
 							</tr>
 						</thead>
 						<tbody>
-							{uniqueTimeLabels.map((labelInfo) => (
-								<tr key={labelInfo.time}>
+							{uniqueTimeLabels.map((labelInfo, timeIndex) => (
+								<tr key={timeIndex}>
 									<td className="sticky left-0 z-10 bg-card border border-border p-3">
 										<span
 											className={cn(
@@ -248,10 +248,7 @@ export function AvailabilityGrid({
 										</span>
 									</td>
 									{Array.from(slotsByDate.entries()).map(([date, slots]) => {
-										const slotIndex = uniqueTimeLabels.findIndex(
-											(l) => l.time === labelInfo.time,
-										);
-										const slot = slots[slotIndex];
+										const slot = slots[timeIndex];
 										if (!slot)
 											return <td key={date} className="border border-border" />;
 
